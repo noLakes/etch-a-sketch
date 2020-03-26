0e6562e892b7c43fd124f3cc373da559b0eedaf8
“noLakes” <sricho93@gmail.com>
2020-03-26 17:40:27 -0400

    Added a grid resize button that allows used to enter new grid dimensions.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let cellHeight = 0;
 
 function createGrid(width, height) {
     cellWidth = 720/width - 2;
-    cellHeight = 720/height - 2;
+    cellHeight = 656/height - 2;
     for (i = 1; i <= (width * height); i++) {
         let gridCell = document.createElement('div');
         gridCell.setAttribute("id", "gridCell-" + i)
@@ -18,13 +18,28 @@ function createGrid(width, height) {
     } 
 }
 
-createGrid(32, 32);
+createGrid(16, 16);
 
-const button = document.createElement("button");
-button.setAttribute("id", "button");
-button.textContent = "Clear";
 const body = document.querySelector("body");
-body.appendChild(button);
+const gridButton = document.createElement("button");
+gridButton.setAttribute("id", "gridButton");
+gridButton.textContent = "Grid Size";
+
+gridButton.addEventListener("click", function(){
+    let newGridDimensions = prompt("New grid dimensions?");
+    if (newGridDimensions != null && newGridDimensions != "") {
+        let child = grid.lastElementChild;
+        while (child) {
+            grid.removeChild(child);
+            child = grid.lastElementChild;
+        }
+        createGrid(newGridDimensions, newGridDimensions);
+    }
+    })
+
+body.appendChild(gridButton);
+
+
 
 function count(selector) {
     return selector.childElementCount;
